ee3bbe22d4d98f7a600202308ab3486dcbdf5f06
Brian Mannakee <mannakee@email.arizona.edu>
2018-04-19 14:44:32 -0700

    updates

--- analyze_snp_calls.R
+++ analyze_snp_calls.R
@@ -1,11 +1,41 @@
 library(tidyverse)
 library(smartcallr)
 library(BSgenome.Hsapiens.1000genomes.hs37d5)
-load('./data/platinum_snps.RDA')
-#fr <- load_variants('./data/aml31R_mutect2.filtered.vcf',BSgenome.Hsapiens.1000genomes.hs37d5,sample_name="aml31R")
-fr <- compute_odds('./data/aml31R_mutect2.filtered.vcf',BSgenome.Hsapiens.1000genomes.hs37d5,sample_name="aml31R")
+library(precrec)
+load('./data/gold_snps.RDA')
+load('./markers.RDA')
+markers$start <- markers$end # they are off by one, for reasons I can not explain!
+fr <- smartcallr::compute_odds('./data/aml31P_mutect2.filtered.vcf',BSgenome.Hsapiens.1000genomes.hs37d5,sample_name="aml31P",prior_lod=50)
 fr$chrom <- as.character(fr$seqnames)
 fr$start <- as.character(fr$start)
+fr$end <- as.character(fr$end)
  
-joined <- fr %>% dplyr::full_join(platinum_snps,by=c("chrom"="chromosome_name","start"="start"))
-joined <- joined %>% dplyr::select(RELAPSE_WGS_VAF,RELAPSE_ALLDNA_VAF,mutect_odds,prior_odds,softFilterMatrix.clustered_events,pass_all,tlod_only,chrom,start,everything())
+joined <- fr %>% dplyr::full_join(gold_snps,by=c("chrom"="chromosome_name","start"="start","end"="stop","ref"="reference","alt"="variant"))
+joined <- joined %>% dplyr::full_join(markers,by=c("chrom"="chr","start"="start","ref"="ref","alt"="var"))
+joined <- joined %>% dplyr::select("tumor_capture_vaf"=TUMOR_CAPTURE_VAF,'relapse_capture_vaf'='RELAPSE_CAPTURE_VAF',tumor_vaf,relapse_vaf,normal_vaf,default_gene_name,TLOD,AF,mutect_odds,prior_odds,softFilterMatrix.clustered_events,pass_all,tlod_only,chrom,start,everything())
+
+# both_pass_and_found <- joined %>% dplyr::filter(mutect_odds > 2 & prior_odds > 2 & !is.na(RELAPSE_WGS_VAF))
+# both_fail_and_found <- joined %>% dplyr::filter(mutect_odds < 2 & prior_odds < 2 & !is.na(RELAPSE_WGS_VAF))
+# mutect_pass_prior_fail_and_found <- joined %>% dplyr::filter(mutect_odds >= 2 & prior_odds < 2 & !is.na(RELAPSE_WGS_VAF))
+# mutect_fail_prior_pass_and_found <- joined %>% dplyr::filter(mutect_odds < 2 & prior_odds >= 2 & !is.na(RELAPSE_WGS_VAF))
+# both_pass_and_not_found <- joined %>% dplyr::filter(mutect_odds > 2 & prior_odds > 2 & is.na(RELAPSE_WGS_VAF))
+
+# called_by_them_not_seen_mutect <- joined %>% dplyr::filter(is.na(sampleNames) & as.numeric(RELAPSE_WGS_VAF) > 0.0) # n=33
+# called_by_mutect_not_seen_them <- joined %>% dplyr::filter(pass_all & is.na(RELAPSE_WGS_VAF)) %>% dplyr::select(start) # n=1108. No idea why. for the first one, 1:5100397 I don't see any reads, but I might have the wrong file!
+# mutect_tlod_only_called_them <- joined %>% dplyr::filter(tlod_only & as.numeric(RELAPSE_ALLDNA_VAF) > 0.0) # n=36 none elevated
+# mutect_pass_all_called_them <- joined %>% dplyr::filter(pass_all & as.numeric(RELAPSE_ALLDNA_VAF) > 0.0) # n=36 none elevated
+# 
+# prior_elevated <- joined %>% dplyr::filter(tlod_only & prior_odds > 2) # n=0
+# prior_reduced <- joined %>% dplyr::filter(pass_all & prior_odds < 2)
+# prior_wtf <- joined %>% dplyr::filter(pass_all & prior_odds > 2 & is.na(RELAPSE_WGS_VAF))
+truth_table <- joined %>% dplyr::filter(!is.na(tumor_vaf) & (pass_all | tlod_only)) %>%
+  dplyr::mutate(present=(!is.na(tumor_capture_vaf) & tumor_capture_vaf > 0.0),
+                                        not_present=(is.na(tumor_capture_vaf) | tumor_capture_vaf==0.0)) %>%
+  dplyr::select(present,not_present,everything())
+
+sscurves_mutect <- precrec::evalmod(scores=truth_table$mutect_odds,labels=truth_table$present)
+sscurves_prior <-  precrec::evalmod(scores=truth_table$prior_odds,labels=truth_table$present)
+
+p1 <- ggplot(joined,aes(x=log10(mutect_odds),y=log10(prior_odds))) + geom_point() + xlim(c(-3,3)) + ylim(c(-3,3))
+
+prior_density_plot <- ggplot(joined,aes(logit_prior)) + geom_density() +  geom_vline(xintercept=-6) + theme_classic() 
--- load_and_clean_gold_standard_snps.R
+++ load_and_clean_gold_standard_snps.R
@@ -4,3 +4,13 @@ platinum_snps <- readr::read_tsv('./data/platinum_list.tsv',col_types=cols(.defa
 save(platinum_snps,file='./data/platinum_snps.RDA')
 gold_snps <- readr::read_tsv('./data/gold_snps.tsv',col_types=cols(.default = 'c'))
 save(gold_snps,file='./data/gold_snps.RDA')
+# Load and clean complete marker set and readcounts
+markers <- readr::read_tsv('./data/Supplemental_Dataset_2-SomaticSnvsTargetedForValidation-ReadCounts.tsv',col_types=cols(.default= 'c')) %>%
+  mutate(tumor_vaf=as.numeric(`tumor vaf`),
+         relapse_vaf=as.numeric(`relapse vaf`),
+         normal_vaf=as.numeric(`normal vaf`),
+         normal_depth=as.numeric(`normal depth`),
+         tumor_depth=as.numeric(`tumor depth`),
+         relapse_depth=as.numeric(`relapse depth`)) %>%
+  dplyr::select(tumor_vaf,relapse_vaf,normal_vaf,normal_depth,tumor_depth,relapse_depth,everything())
+save(markers,file='markers.RDA')
